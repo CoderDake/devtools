ae3562cdfc8f2ab38e0c416afcf865604ea9f3c0
Dan Chevalier <chevalier.dan@gmail.com>
2022-05-25 09:57:36 -0400

    Try running some full tests now

--- packages/devtools_app/test_test.sh
+++ packages/devtools_app/test_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 
-COUNT=100
+COUNT=50
 TEST_NAME=$1
-NOW=$(date +"%Y-%m-%d-%s")
+NOW=$(date +"%Y-%m-%d--%H-%M-%S")
 TEST_DIR=.test/${NOW}_${TEST_NAME}
 PASS_DIR=$TEST_DIR/passes
 FAIL_DIR=$TEST_DIR/fails
@@ -20,7 +20,7 @@ mkdir -p $FAIL_DIR
   for (( i=1; i<=$COUNT; i++ ))
   do
     TEST_NAME=.tmp_test.log
-    flutter test test/performance/performance_controller_test.dart test/inspector/inspector_integration_test.dart > $TEST_NAME
+    flutter test test/ > $TEST_NAME
     if [ $? -eq 0 ]; then
       PASS_COUNT=$((PASS_COUNT+1))
       mv $TEST_NAME $PASS_DIR/test-$i.log
