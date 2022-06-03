8c1f9f4a58a8228d5a519248dcf451bd86f48d9d
Dan Chevalier <chevalier.dan@gmail.com>
2022-06-03 17:01:47 -0400

    150 times

---
+++ packages/devtools_app/.test/2022-06-03--16-44-06_/test_test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -x
+
+COUNT=50
+TEST_NAME=$1
+NOW=$(date +"%Y-%m-%d--%H-%M-%S")
+TEST_DIR=.test/${NOW}_${TEST_NAME}
+PASS_DIR=$TEST_DIR/passes
+FAIL_DIR=$TEST_DIR/fails
+
+PASS_COUNT=0
+FAIL_COUNT=0
+
+mkdir -p $PASS_DIR
+mkdir -p $FAIL_DIR
+((
+  flutter clean
+
+  cp test_test.sh $TEST_DIR/
+
+  for (( i=1; i<=$COUNT; i++ ))
+  do
+    SEED=$RANDOM
+    TEST_NAME=.tmp_test.log
+    echo "SEED: $SEED"
+    flutter test test/ --test-randomize-ordering-seed $SEED> $TEST_NAME
+    if [ $? -eq 0 ]; then
+      PASS_COUNT=$((PASS_COUNT+1))
+      mv $TEST_NAME $PASS_DIR/test-$i.log
+    else
+      FAIL_COUNT=$((FAIL_COUNT+1))
+      mv $TEST_NAME $FAIL_DIR/test-$i.log
+    fi
+    echo "PASSES: $PASS_COUNT FAILS: $FAIL_COUNT"
+    rm -rf /tmp/flutter_tools*
+  done
+) 2>&1) | tee $TEST_DIR/log.log
