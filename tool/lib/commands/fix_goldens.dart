aa17a2dc1a24356fa4827bd5b3f398830ecf3820
Dan Chevalier <chevalier.dan@gmail.com>
2023-09-12 11:05:53 -0400

    cleanup tmp dir

--- tool/lib/commands/fix_goldens.dart
+++ tool/lib/commands/fix_goldens.dart
@@ -35,75 +35,75 @@ class FixGoldensCommand extends Command {
     final runId = argResults!['run-id']!;
 // DOWNLOAD_DIR=$(mktemp -d)
     final tmpDownloadDir = Directory(
-      path.join('.tmp', '${DateTime.now().millisecondsSinceEpoch}'),
+      path.join('.tmp${DateTime.now().millisecondsSinceEpoch}'),
     );
     tmpDownloadDir.createSync();
-
+    try {
 // echo "Downloading the artifacts to $DOWNLOAD_DIR"
 // gh run download $RUN_ID -p "*golden_image_failures*" -D "$DOWNLOAD_DIR"
-    await processManager.runProcess(
-      CliCommand.from('gh', [
-        'run',
-        'download',
-        runId,
-        '-p',
-        '*golden_image_failures*',
-        '-D',
-        tmpDownloadDir.path,
-      ]),
-    );
+      await processManager.runProcess(
+        CliCommand.from('gh', [
+          'run',
+          'download',
+          runId,
+          '-p',
+          '*golden_image_failures*',
+          '-D',
+          tmpDownloadDir.path,
+        ]),
+      );
 
 // NEW_GOLDENS=$(find $DOWNLOAD_DIR -type f | grep "testImage.png" )
-    final newGoldens = tmpDownloadDir
-        .listSync(recursive: true)
-        .where((e) => e.path.endsWith('testImage.png'));
-    print(newGoldens);
+      final newGoldens = tmpDownloadDir
+          .listSync(recursive: true)
+          .where((e) => e.path.endsWith('testImage.png'));
+      print(newGoldens);
 // cd packages/devtools_app/test/
-    final allDevtoolsPngFiles =
-        Directory(pathFromRepoRoot("packages/devtools_app/test/"))
-            .listSync(recursive: true)
-          ..where((e) => e.path.endsWith('.png'));
+      final allDevtoolsPngFiles =
+          Directory(pathFromRepoRoot("packages/devtools_app/test/"))
+              .listSync(recursive: true)
+            ..where((e) => e.path.endsWith('.png'));
 
-    print('NewGoldens L: ${newGoldens.length}');
+      print('NewGoldens L: ${newGoldens.length}');
 
-    for (final file in newGoldens) {
+      for (final file in newGoldens) {
 // while IFS= read -r GOLDEN ; do
 //   FILE_NAME=$(basename $GOLDEN | sed "s|_testImage.png$|.png|")
-      final baseName = path.basename(file.path);
-      final pngRoot =
-          '${RegExp(r'(^.*)_testImage.png').firstMatch(baseName)?.group(1)}.png';
+        final baseName = path.basename(file.path);
+        final pngRoot =
+            '${RegExp(r'(^.*)_testImage.png').firstMatch(baseName)?.group(1)}.png';
 
-      print('basename: $baseName root: $pngRoot');
-      final fileMatches = allDevtoolsPngFiles.where(
-        (e) => e.path.endsWith(pngRoot),
-      );
-      print('MATCHES: $fileMatches');
+        print('basename: $baseName root: $pngRoot');
+        final fileMatches = allDevtoolsPngFiles.where(
+          (e) => e.path.endsWith(pngRoot),
+        );
+        print('MATCHES: $fileMatches');
 //   FOUND_FILES=$(find . -name "$FILE_NAME" )
 //   FOUND_FILE_COUNT=$(echo -n "$FOUND_FILES" | grep -c '^')
 
 //   if [[ $FOUND_FILE_COUNT -ne 1 ]] ; then
-      final String destinationPath;
-      if (fileMatches.isEmpty) {
-        throw 'Could not find a golden Image for $baseName using $pngRoot as '
-            'the item of the search.';
-      } else if (fileMatches.length == 1) {
-        destinationPath = fileMatches.first.path;
-      } else {
-        print("Multiple goldens found for ${file.path}");
-        print("Select which golden should be overridden:");
+        final String destinationPath;
+        if (fileMatches.isEmpty) {
+          throw 'Could not find a golden Image for $baseName using $pngRoot as '
+              'the item of the search.';
+        } else if (fileMatches.length == 1) {
+          destinationPath = fileMatches.first.path;
+        } else {
+          print("Multiple goldens found for ${file.path}");
+          print("Select which golden should be overridden:");
 
-        for (int i = 0; i < fileMatches.length; i++) {
-          final fileMatch = fileMatches.elementAt(i);
-          print('${i + 1}) ${fileMatch.path}');
+          for (int i = 0; i < fileMatches.length; i++) {
+            final fileMatch = fileMatches.elementAt(i);
+            print('${i + 1}) ${fileMatch.path}');
+          }
+
+          final userSelection = int.parse(stdin.readLineSync()!);
+
+          destinationPath = fileMatches.elementAt(userSelection - 1).path;
         }
+        await file.rename(destinationPath);
 
-        final userSelection = int.parse(stdin.readLineSync()!);
-
-        destinationPath = fileMatches.elementAt(userSelection - 1).path;
-      }
-      await file.rename(destinationPath);
-
-      print("Fixed: $destinationPath");
+        print("Fixed: $destinationPath");
 //     # If there are goldens with conflicting names, we need to pick which one
 //     # maps to the artifact.
 //     echo "Multiple goldens found for $(echo $GOLDEN| sed 's|^.*golden_image_failures[^/]*/||')"
@@ -121,9 +121,12 @@ class FixGoldensCommand extends Command {
 //   echo "FIXED: $DEST_PATH"
 //   mv "$GOLDEN" "$DEST_PATH"
 // done <<< "$NEW_GOLDENS"
-    }
+      }
 
 // echo "Done updating $(echo -n "$NEW_GOLDENS" | grep -c '^') goldens"
-    print('Done updating ${newGoldens.length} goldens');
+      print('Done updating ${newGoldens.length} goldens');
+    } finally {
+      tmpDownloadDir.deleteSync(recursive: true);
+    }
   }
 }
